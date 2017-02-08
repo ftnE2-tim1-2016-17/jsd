ece75f9a5d89629c8a969c54fd9f9bb57d2c8480
Milana Dapra <milana.dapra@gmail.com>
2017-02-08 13:34:30 +0100

    Grammar and test file added.

---
+++ textXGrammar/query.py
@@ -0,0 +1,8 @@
+from textx.metamodel import metamodel_from_file
+from textx.export import metamodel_export, model_export
+import pydot
+from textx.exceptions import TextXSemanticError
+
+
+class Query(object):
+    pass
