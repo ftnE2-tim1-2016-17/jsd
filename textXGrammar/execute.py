f05945b1f2e0e6d08655d643fcdb929b8b9bb9a7
Bosko Sogura <bolesogura93@gmail.com>
2017-03-05 21:09:08 +0100

    Interpreter za novu gramatiku.

--- textXGrammar/execute.py
+++ textXGrammar/execute.py
@@ -7,24 +7,171 @@ import datetime
 
 class Query(object):
 
-    '''def interpreter(self, model):
+    def __init__(self):
+        self.query_set = {}
 
-        self.city = model.city
+    def interpreter(self, model):
 
-        self.dayFrom = model.dateFrom.day.number
-        self.monthFrom = model.dateFrom.month.number
-        self.yearFrom = model.dateFrom.year.number
-        self.hourFrom = model.timeFrom.hour.number
-        self.minuteFrom = model.timeFrom.minute.number
+        service_type = model.serviceType
+        service_item = model.serviceItem.car
 
-        self.dayTo = model.dateTo.day.number
-        self.monthTo = model.dateTo.month.number
-        self.yearTo = model.dateTo.year.number
-        self.hourTo = model.timeTo.hour.number
-        self.minuteTo = model.timeTo.minute.number
+        self.query_set["serviceType"] = service_type
+        self.query_set["serviceItem"] = service_item
+        parameters = model.parameters
+        date_from_check = False
+        time_from_check = False
+        date_to_check = True
+        time_to_check = True
 
-        return self '''
+        for param in parameters:
+            if param.city is not None:
+                city = param.city
+                self.query_set["city"] = city
+            if param.dateFrom is not None:
+                date_from_check = True
+                date_to_check = False
+                date_from = param.dateFrom
+                day_from = int(date_from.dateFrom.day.number)
+                if date_from.dateFrom.month is not None:
+                    month_from = int(date_from.dateFrom.month.number)
+                    if (month_from == 1) | (month_from == 3) | (month_from == 5) | (month_from == 7) | (month_from == 8)\
+                            | (month_from == 10) | (month_from == 12):
+                        if (day_from < 1) | (day_from > 30):
+                            raise ValueError("Uneli ste nepostojeci dan za datum iznajmljivanja vozila.")
+                    elif month_from == 2:
+                        if (day_from < 1) | (day_from > 28):
+                            raise ValueError("Uneli ste nepostojeci dan za datum iznajmljivanja vozila.")
+                    elif (month_from == 4) | (month_from == 6) | (month_from == 9) | (month_from == 11):
+                        if (day_from < 1) | (day_from > 30):
+                            raise ValueError("Uneli ste nepostojeci dan za datum iznajmljivanja vozila.")
+                    else:
+                        raise ValueError("Uneli ste nepostojeci mjesec za datum iznajmljivanja vozila.")
+                else:
+                    month_from = datetime.datetime.now().month
+                if date_from.dateFrom.year is not None:
+                    year_from = int(date_from.dateFrom.year.number)
+                    if year_from < datetime.datetime.now().year:
+                        raise ValueError("Godina iznajmljivanja ne moze biti manja od tekuce.")
+                else:
+                    year_from = datetime.datetime.now().year
+                if year_from == datetime.datetime.now().year:
+                    if month_from < datetime.datetime.now().month:
+                        raise ValueError("Mjesec iznajmljivanja ne moze biti manji od tekuceg.")
+                    elif month_from == datetime.datetime.now().month:
+                        if day_from < datetime.datetime.now().day:
+                            raise ValueError("Dan iznajmljivanja ne moze biti manji od tekuceg.")
+                self.query_set["dayFrom"] = day_from
+                self.query_set["monthFrom"] = month_from
+                self.query_set["yearFrom"] = year_from
 
+            if not date_from_check:
+                date_from_check = True
+                date_to_check = False
+                day_from = datetime.datetime.now().day
+                month_from = datetime.datetime.now().month
+                year_from = datetime.datetime.now().year
+                self.query_set["dayFrom"] = day_from
+                self.query_set["monthFrom"] = month_from
+                self.query_set["yearFrom"] = year_from
+
+            if param.timeFrom is not None:
+                time_from = param.timeFrom
+                time_from_check = True
+                time_to_check = False
+                hour_from = int(time_from.timeFrom.hour.number)
+                self.query_set["hourFrom"] = hour_from
+                if time_from.timeFrom.minute is not None:
+                    minute_from = int(time_from.timeFrom.minute.number)
+                else:
+                    minute_from = 0
+                self.query_set["minuteFrom"] = minute_from
+            if not time_from_check:
+                time_from_check = True
+                time_to_check = False
+                hour_from = int(datetime.datetime.now().time().hour + 1)
+                minute_from = 30
+                self.query_set["hourFrom"] = hour_from
+                self.query_set["minuteFrom"] = minute_from
+
+            if param.dateTo is not None:
+                date_to_check = True
+                date_to = param.dateTo
+                day_to = int(date_to.dateTo.day.number)
+                if date_to.dateTo.month is not None:
+                    month_to = int(date_to.dateTo.month.number)
+                    if (month_to == 1) | (month_to == 3) | (month_to == 5) | (month_to == 7) | (month_to == 8) | \
+                            (month_to == 10) | (month_to == 12):
+                        if (day_to < 1) | (day_to > 30):
+                            raise ValueError("Uneli ste nepostojeci dan za datum vracanja vozila.")
+                    elif month_to == 2:
+                        if (day_to < 1) | (day_to > 28):
+                            raise ValueError("Uneli ste nepostojeci dan za datum vracanja vozila.")
+                    elif (month_to == 4) | (month_to == 6) | (month_to == 9) | (month_to == 11):
+                        if (day_to < 1) | (day_to > 30):
+                            raise ValueError("Uneli ste nepostojeci dan za datum vracanja vozila.")
+                    else:
+                        raise ValueError("Uneli ste nepostojeci mjesec za datum vracanja vozila.")
+                else:
+                    month_to = datetime.datetime.now().month
+                if date_to.dateTo.year is not None:
+                    year_to = int(date_to.dateTo.year.number)
+                    if year_to < datetime.datetime.now().year:
+                        raise ValueError("Godina vracanja ne moze biti manja od tekuce.")
+                else:
+                    year_to = datetime.datetime.now().year
+                if year_to == datetime.datetime.now().year:
+                    if month_to < datetime.datetime.now().month:
+                        raise ValueError("Datum vracanja(mjesec) ne moze biti manji od tekuceg.")
+                    elif month_to == datetime.datetime.now().month:
+                        if day_to < datetime.datetime.now().day:
+                            raise ValueError("Datum vracanja(dan) ne moze biti manji od tekuceg.")
+                self.query_set["dayTo"] = day_to
+                self.query_set["monthTo"] = month_to
+                self.query_set["yearTo"] = year_to
+            if not date_to_check:
+                date_to_check = True
+                day_to = self.query_set["dayFrom"] + 1
+                month_to = datetime.datetime.now().month
+                year_to = datetime.datetime.now().year
+                self.query_set["dayTo"] = day_to
+                self.query_set["monthTo"] = month_to
+                self.query_set["yearTo"] = year_to
+
+            if param.timeTo is not None:
+                time_to = param.timeTo
+                time_to_check = True
+                hour_to = int(time_to.timeTo.hour.number)
+                self.query_set["hourTo"] = hour_to
+                if time_to.timeTo.minute is not None:
+                    minute_to = int(time_to.timeTo.minute.number)
+                else:
+                    minute_to = 0
+                self.query_set["minuteTo"] = minute_to
+            if not time_to_check:
+                time_from_check = True
+                hour_to = self.query_set["hourFrom"]
+                minute_to = self.query_set["minuteFrom"]
+                self.query_set["hourTo"] = hour_to
+                self.query_set["minuteTo"] = minute_to
+
+            if param.itemParameters is not None:
+                if param.itemParameters.carParameters is not None:
+                    for itemParam in param.itemParameters.carParameters:
+                        if itemParam.carBrand is not None:
+                            carBrand = itemParam.carBrand
+                        if itemParam.fuelType is not None:
+                            fuelType = itemParam.fuelType
+                        if itemParam.gearbox is not None:
+                            gearbox = itemParam.gearbox
+                        if itemParam.carClass is not None:
+                            carClass = itemParam.carClass
+                        if itemParam.priceFrom is not None:
+                            priceFrom = itemParam.priceFrom
+                        if itemParam.priceTo is not None:
+                            priceTo = itemParam.priceTo
+
+        print(self.query_set)
+        return self.query_set
 
 
 def execute(path, grammar_file_name, example_file_name, export_dot, export_png):
@@ -50,9 +197,10 @@ def execute(path, grammar_file_name, example_file_name, export_dot, export_png):
         graph = pydot.graph_from_dot_file(model_name + '.dot')
         graph[0].write_png(model_name + '.png')
 
-    #query = Query()
-    #query_set = query.interpreter(model)
-
+    query = Query()
+    query_set = query.interpreter(model)
+    #provjeriri za granicne uslove kod datuma iznajmjivanja i vrcanja vozila,
+    #kao i poredjenje datuma iznajmljivanja < datuma vracanja
     #return query_set
 
 
