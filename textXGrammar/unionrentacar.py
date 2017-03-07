e6b31df223dd28705ceda0327dc7869b9ef34059
Milana Dapra <milana.dapra@gmail.com>
2017-03-07 15:38:27 +0100

    unionrentacar klase

--- textXGrammar/unionrentacar.py
+++ textXGrammar/unionrentacar.py
@@ -1,19 +1,110 @@
 import urllib.request
 
-def make_requests():
-    response = [None]
 
-    if request_www_unionrentacar_com(response):
+def make_requests(model):
+    response = [None]
+    if request_www_unionrentacar_com(response, model):
         html_response = response[0].read()
         print(html_response)
         response[0].close()
+        return html_response
+
+cities = {
+    "beograd": 3,
+    "novi sad": 5,
+    "nis": 3
+}
+car_brands = {
+    "alfa romeo": 30,
+    "audi": 24,
+    "bmw": 4,
+    "chevrolet": 12,
+    "citroen": 19,
+    "dacia": 14,
+    "fiat": 7,
+    "ford": 26,
+    "honda": 23,
+    "hyundai": 21,
+    "jaguar": 41,
+    "jeep": 34,
+    "kia": 33,
+    "lancia": 31,
+    "mazda": 39,
+    "mercedes": 5,
+    "nissan": 22,
+    "opel": 16,
+    "peugeot": 3,
+    "renault": 6,
+    "seat": 17,
+    "skoda": 15,
+    "suzuki": 25,
+    "volkswagen": 18,
+    "vw": 18,
+    "volvo": 37
+}
+fuel_types = {
+    "benzin": 1,
+    "plin": 8,
+    "benzin plin": 8,
+    "eurodizel": 9,
+    "dizel": 2
+}
+gearboxes = {
+    "automatik": 5,
+    "manuelni": 6,
+    "poluautomatik": 7
+}
+car_classes = {
+    "gradski": 13,
+    "niska klasa": 14,
+    "srednja klasa": 15,
+    "visoka klasa": 17,
+    "luksuzni": 18,
+    "skuter": 24,
+    "limuzina": 25,
+    "kombi": 26
+}
 
 
-def request_www_unionrentacar_com(response):
+def request_www_unionrentacar_com(response, model):
     response[0] = None
 
     try:
-        req = urllib.request.Request("https://www.unionrentacar.com/search?pickup_town=0&same_location=on&dropoff_town=0&dropoff_location=0&pickup_date=06&pickup_month=03.2017&pickup_hour=18&pickup_minutes=30&dropoff_date=07&dropoff_month=03.2017&dropoff_hour=18&dropoff_minutes=30&car_brand=0&car_model=&fuel_type=0&gearshift_type=0&car_class%5B%5D=0&price_from=0&price_to=0&payway=0&deposit=0&mileage=0&search_submit=&action=search&sidebar_=sidebar_&advanced_search=0&puDay=06&puMonth=03&puYear=2017&doDay=07&doMonth=03&doYear=2017&pickup_location=")
+        req = urllib.request.Request(
+            "https://www.unionrentacar.com/search?"
+            "pickup_town=0&"
+            "same_location=on&"
+            "dropoff_town=0&"
+            "dropoff_location=0&"
+            "pickup_date=06&"
+            "pickup_month=03.2017&"
+            "pickup_hour=18&"
+            "pickup_minutes=30&"
+            "dropoff_date=07&"
+            "dropoff_month=03.2017&"
+            "dropoff_hour=18&"
+            "dropoff_minutes=30&"
+            "car_brand=0&"
+            "car_model=&"
+            "fuel_type=0&"
+            "gearshift_type=0&"
+            "car_class%5B%5D=0&"
+            "price_from=0&"
+            "price_to=0&"
+            "payway=0&"
+            "deposit=0&"
+            "mileage=0&"
+            "search_submit=&"
+            "action=search&"
+            "sidebar_=sidebar_&"
+            "advanced_search=0&"
+            "puDay=06&"
+            "puMonth=03&"
+            "puYear=2017&"
+            "doDay=07&"
+            "doMonth=03&"
+            "doYear=2017&"
+            "pickup_location=")
 
         req.add_header("Connection", "keep-alive")
         req.add_header("Upgrade-Insecure-Requests", "1")
